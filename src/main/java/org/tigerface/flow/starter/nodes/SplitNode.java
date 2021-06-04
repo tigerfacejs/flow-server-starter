af2536e7e99553af43caf033e13a696afe7717ad
Tiger <zhangyihu@gmail.com>
2021-06-04 18:09:33 +0800

    实现 split aggregate

--- src/main/java/org/tigerface/flow/starter/nodes/SplitNode.java
+++ src/main/java/org/tigerface/flow/starter/nodes/SplitNode.java
@@ -3,41 +3,44 @@ package org.tigerface.flow.starter.nodes;
 import groovy.lang.GroovyClassLoader;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.AggregationStrategy;
+import org.apache.camel.Expression;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.ProcessorDefinition;
 
 import java.util.Map;
 
 @Slf4j
-public class EnrichNode implements IFlowNode {
+public class SplitNode implements IFlowNode {
     RouteBuilder builder;
 
-    public EnrichNode(RouteBuilder builder) {
+    public SplitNode(RouteBuilder builder) {
         this.builder = builder;
     }
 
     @Override
-    public <T extends ProcessorDefinition<T>> T createAndAppend(Map<String, Object> node, T rd) {
+    public <T extends ProcessorDefinition<T>>T createAndAppend(Map<String, Object> node, T rd) {
         Map<String, Object> props = (Map<String, Object>) node.get("props");
-        String uri = (String) props.get("uri");
-        String script = (String)(props.get("aggregationStrategy") != null ? props.get("aggregationStrategy") : props.get("script"));
 
-        if (script != null && script.length() > 0) {
+        String aggregationStrategy = (String) props.get("aggregationStrategy");
+        Expression splitExp = Exp.create((Map) props.get("split"));
+
+        if (aggregationStrategy != null && aggregationStrategy.length() > 0) {
             final ClassLoader tccl = Thread.currentThread().getContextClassLoader();
             final GroovyClassLoader groovyClassLoader = new GroovyClassLoader(tccl);
-            Class aggregationStrategyClazz = groovyClassLoader.parseClass(script);
+            Class aggregationStrategyClazz = groovyClassLoader.parseClass(aggregationStrategy);
 
             try {
                 Object aggregator = aggregationStrategyClazz.newInstance();
-                rd.enrich(uri, (AggregationStrategy) aggregator);
+                rd.split(splitExp, (AggregationStrategy) aggregator);
             } catch (Exception e) {
                 e.printStackTrace();
                 throw new RuntimeException("解析动态 AggregationStrategy 聚合策略脚本失败");
             }
         } else {
-            rd.enrich(uri);
+            rd.split(splitExp);
         }
-        log.info("创建 enrich 节点");
+
+        log.info("创建 Split 节点");
 
         return rd;
     }
