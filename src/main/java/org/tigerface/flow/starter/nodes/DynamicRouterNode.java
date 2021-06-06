38027d80c4ae850371ed3c6dd3ce22759b31b05a
zhangyihu <zhangyihu@gmail.com>
2021-06-06 11:37:19 +0800

    增加 Multicast

--- src/main/java/org/tigerface/flow/starter/nodes/DynamicRouterNode.java
+++ src/main/java/org/tigerface/flow/starter/nodes/DynamicRouterNode.java
@@ -1,10 +1,14 @@
 package org.tigerface.flow.starter.nodes;
 
+import groovy.lang.GroovyClassLoader;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.AggregationStrategy;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.ProcessorDefinition;
 
 import java.util.Map;
+
+import static org.apache.camel.builder.Builder.method;
 
 @Slf4j
 public class DynamicRouterNode implements IFlowNode {
@@ -17,8 +21,21 @@ public class DynamicRouterNode implements IFlowNode {
     @Override
     public <T extends ProcessorDefinition<T>>T createAndAppend(Map<String, Object> node, T rd) {
         Map<String, Object> props = (Map<String, Object>) node.get("props");
+        String script = (String) (props.get("dynamicRouter") != null ? props.get("dynamicRouter") : props.get("script"));
 
-        rd.dynamicRouter(Exp.create(props));
+        if (script != null && script.length() > 0) {
+            final ClassLoader tccl = Thread.currentThread().getContextClassLoader();
+            final GroovyClassLoader groovyClassLoader = new GroovyClassLoader(tccl);
+            Class dynamicRouterClazz = groovyClassLoader.parseClass(script);
+
+            try {
+                rd.dynamicRouter(method(dynamicRouterClazz, "router"));
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new RuntimeException("解析 DynamicRouter Bean 脚本失败");
+            }
+        } else throw new RuntimeException("DynamicRouterNode 必须是 Bean ，包含名为 router 的方法，且缺省返回 null");
+
         log.info("创建 dynamicRouter 节点");
 
         return rd;
---
+++ src/main/java/org/tigerface/flow/starter/nodes/MulticastNode.java
@@ -0,0 +1,68 @@
+package org.tigerface.flow.starter.nodes;
+
+import groovy.lang.GroovyClassLoader;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.AggregationStrategy;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.MulticastDefinition;
+import org.apache.camel.model.ProcessorDefinition;
+
+import java.util.List;
+import java.util.Map;
+
+/*
+    {
+        "type": "multicast",
+        "props": {
+            "timeout": "500",
+            "multicast": ["direct:a", "direct:b", "direct:c"]
+        }
+    }
+*/
+@Slf4j
+public class MulticastNode implements IFlowNode {
+    RouteBuilder builder;
+
+    public MulticastNode(RouteBuilder builder) {
+        this.builder = builder;
+    }
+
+    @Override
+    public <T extends ProcessorDefinition<T>> T createAndAppend(Map<String, Object> node, T rd) {
+        Map<String, Object> props = (Map<String, Object>) node.get("props");
+        String timeout = (String) props.get("timeout");
+        List<String> multicast = (List<String>) props.get("multicast");
+
+        if (multicast != null && multicast.size() > 0) {
+            MulticastDefinition md;
+            String script = (String) (props.get("aggregationStrategy") != null ? props.get("aggregationStrategy") : props.get("script"));
+
+            if (script != null && script.length() > 0) {
+                final ClassLoader tccl = Thread.currentThread().getContextClassLoader();
+                final GroovyClassLoader groovyClassLoader = new GroovyClassLoader(tccl);
+                Class aggregationStrategyClazz = groovyClassLoader.parseClass(script);
+
+                try {
+                    Object aggregator = aggregationStrategyClazz.newInstance();
+                    md = rd.multicast((AggregationStrategy) aggregator);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new RuntimeException("解析动态 AggregationStrategy 聚合策略脚本失败");
+                }
+            } else {
+                md = rd.multicast();
+            }
+
+            md.stopOnException();
+            md.parallelProcessing();
+            md.timeout(timeout != null ? Long.valueOf(timeout) : 500);
+
+            for (String uri : multicast) {
+                md.toD(uri);
+            }
+        }
+
+        log.info("创建 multicast 节点");
+        return rd;
+    }
+}
