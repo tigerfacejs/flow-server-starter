9150f235c01d5c7dd191265b7423de08467e3818
Tiger <zhangyihu@gmail.com>
2021-07-28 14:07:36 +0800

    添加 derby 支持，开发中有bug

--- src/main/java/org/tigerface/flow/starter/config/FlowServerAutoConfig.java
+++ src/main/java/org/tigerface/flow/starter/config/FlowServerAutoConfig.java
@@ -2,13 +2,18 @@ package org.tigerface.flow.starter.config;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.language.groovy.GroovyShellFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.tigerface.flow.starter.route.SystemRoutes;
 import org.tigerface.flow.starter.service.DeployService;
 import org.tigerface.flow.starter.service.FlowBuilder;
 import org.tigerface.flow.starter.service.PluginManager;
 import org.tigerface.flow.starter.service.TerminalService;
+
+import javax.sql.DataSource;
 
 @Configuration
 //@ConditionalOnClass(RestRoute.class)
@@ -40,4 +45,19 @@ public class FlowServerAutoConfig {
     public RouteBuilder restRoute() {
         return new SystemRoutes();
     }
+
+    @Primary
+    @Bean(name = "dataSource")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+//    @Bean(name = "flowDS")
+//    public DataSource flowDS() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.driverClassName("org.apache.derby.jdbc.EmbeddedDriver");
+//        builder.url("jdbc:derby:derbyDB;create=true");
+//        return builder.build();
+//    }
 }
